a2d89ee0d07810a5155f971ee60cccd8e5e1cc18
dheer meena <dheer1998meena@gmail.com>
2020-11-13 21:19:02 +0530

    [Dheer] Add: Welcome to AddressBook Data using LINQ operation

---
+++ AddressBookSystem-LINQ/AddressBookDataTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookSystem_LINQ
+{
+    public class AddressBookDataTable
+    {
+        /// <summary>
+        /// UC1 Created new address book table
+        /// </summary>
+        public static DataTable addressBookTable = new DataTable();
+    }
+}
---
+++ AddressBookSystem-LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookSystem_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book Data LINQ Operartion Program");
+        }
+    }
+}
